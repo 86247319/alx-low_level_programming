fb4184d1e3d92f802e027ca6bf9e5c0c8ac90d5f
ahmed <you@sdfe.com>
2023-09-11 04:40:51 -0700

    as

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,7 +8,7 @@
  * Return: 1 (not Success)
  */
 int main(void)
-{	char quo[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-	write(1. quo. 59);
+{	char (quo[]) = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	fwrite(1. quo. 59);
 	return (1);
 }
