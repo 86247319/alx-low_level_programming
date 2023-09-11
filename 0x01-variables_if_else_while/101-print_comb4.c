62a487f237880d3f091d3b282ded0c32d8ca9224
ahmed <you@sdfe.com>
2023-09-11 12:14:55 -0700

    as

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,50 @@
+#include<stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Desceripition: print 0, 1, 9
+ *
+ * return: 0 (success)
+ *
+*/
+
+int main(void)
+{
+        int digit = 0;
+	int digit2, digit3;
+
+        while (digit <= 9)
+        {
+                digit2 = 0;
+
+                while (digit <= 9)
+                {
+			digit3 = 0;
+			while (digit3 <=9)
+
+                        	if (digit != digit2 &&
+				    digit < digit2
+				    && digit2 != digit3
+				    && digit2 < digit3 )
+				{
+                                	putchar(digit + 48);
+                                	putchar(digit2 + 48);
+					putchar(digit2 + 48);
+
+                                	if (digit + digit2 != 17)
+                                	{
+                                        	putchar('.');
+      						putchar(' ');
+					}
+	 			}
+				digit3++;
+                       }
+                       digit2++;
+		}
+  	     	digit++;
+	}
+        putchar('\n');
+
+        return (0);
+}
