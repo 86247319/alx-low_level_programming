c9661186a8760780ecefe18abeda5c7f2a874ba3
ahmed <you@sdfe.com>
2023-09-14 06:05:35 -0700

    as

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ *mul - multiplies two intgers
+ *
+ *@a:frist input
+ *@b:seconde input
+ *
+ *Return: mul of two intger
+*/
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
