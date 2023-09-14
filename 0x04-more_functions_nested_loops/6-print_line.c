d7880c37721725fefd34ee1f66f2a1674cf1c785
ahmed <you@sdfe.com>
2023-09-14 12:08:24 -0700

    as

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,23 +1,14 @@
 #include "main.h"
-
 /**
- * print_line - print a straight line
- *
- * @n: is the number of times the - character
- *	should be printed
- *
- */
-
-void print_line(int n);
+*print_line - prints a straight line
+*@n:parameter
+*Return:returns nothing
+*/
+void print_line(int n)
 {
-	int lnChr;
-
-	if (n <= 0)
-		_putchar('\n');
-	else
-	{
-		for (lnChr = 1; lnChr <= n; lnChr++)
-			_putchar(95);
-		_putchar('\n');
-	}
+while (n-- > 0)
+{
+_putchar('_');
+}
+_putchar('\n');
 }
